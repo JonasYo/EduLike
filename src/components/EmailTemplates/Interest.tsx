7dfe41e468f6476b50a4a2ecd330e051dbebb38d
Jonas Marcelino <jonascska07@gmail.com>
2024-11-19 00:47:25 -0300

    feat: added some improvements of design

--- src/components/EmailTemplates/Interest.tsx
+++ src/components/EmailTemplates/Interest.tsx
@@ -1,12 +1,16 @@
 import {
   Body,
+  Column,
   Container,
   Head,
   Heading,
   Hr,
   Html,
+  Img,
+  Link,
   Preview,
-  Tailwind,
+  Row,
+  Section,
   Text,
 } from '@react-email/components';
 
@@ -17,6 +21,8 @@ interface InterestProps {
   phone: string;
   message: string;
 }
+
+const logoUrl = process.env.LOGO_URL;
 
 export const Interest = ({
   subject,
@@ -29,38 +35,185 @@ export const Interest = ({
     <Html>
       <Head />
       <Preview>Novo Interesse Recebido {subject}</Preview>
-      <Tailwind>
-        <Body className="m-auto bg-white font-sans">
-          <Container className="mx-auto my-[40px] w-[465px] rounded-md border border-solid border-slate-300 p-[20px]">
-            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
-              Novo Interesse Recebido: {subject}
-            </Heading>
-            <Text className="text-[14px] leading-[24px] text-black">
-              Olá, Gostaríamos de informar que recebemos um novo interesse em
-              nossos serviços/produtos através do formulário de interesse.
-            </Text>
-            <Text className="text-[14px] leading-[24px] text-black">
-              <strong>Detalhes do Interessado:</strong>
-            </Text>
-            <Text className="text-[14px] leading-[24px] text-black">
-              - Nome: {name} <br />- Email: {email} <br />- Telefone: {phone}{' '}
-              <br />- Mensagem: {message}
-            </Text>
+      <Body style={main}>
+        <Container style={container}>
+          <Section style={logoContainer}>
+            <Img src={logoUrl} width="120" height="36" alt="Likedu" />
+          </Section>
+          <Heading style={h1}>Novo Interesse Recebido: {subject}</Heading>
+          <Text style={heroText}>
+            Recebemos um novo interesse em nossos serviços/produtos através do
+            formulário.
+          </Text>
 
-            <Text className="my-[32px] text-[14px] leading-[24px] text-black">
-              Estamos apurando as informações e entraremos em contato com o
-              cliente o mais rápido possível. Para qualquer questão urgente ou
-              para mais informações, por favor, entre em contato diretamente.
+          <Text className="text-[14px] leading-[24px] text-black">
+            <strong>Detalhes do Interessado:</strong>
+          </Text>
+
+          <Text className="text-[14px] leading-[24px] text-black">
+            - Nome: {name} <br />- Email: {email} <br />- Telefone: {phone}{' '}
+            <br />- Mensagem: {message}
+          </Text>
+
+          <Text className="text-[14px] leading-[24px] text-black">
+            Devemos apurar as informações e entrar em contato com o cliente o
+            mais rápido possível. Para qualquer questão urgente ou para mais
+            informações, por favor, entre em contato diretamente.
+          </Text>
+
+          <Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
+
+          <Text style={text}>Atenciosamente,</Text>
+          <Text style={text}>Likedu</Text>
+
+          <Section>
+            <Row style={footerLogos}>
+              <Column style={{ width: '66%' }}>
+                <Img src={logoUrl} width="120" height="36" alt="Likedu" />
+              </Column>
+              <Column>
+                <Section>
+                  <Row>
+                    <Column>
+                      <Link href="https://likedu.com.br">
+                        <Img
+                          src={`https://cdn.icon-icons.com/icons2/2972/PNG/512/twitter_logo_icon_186891.png`}
+                          width="32"
+                          height="32"
+                          alt="Likedu"
+                          style={socialMediaIcon}
+                        />
+                      </Link>
+                    </Column>
+                    <Column>
+                      <Link href="https://likedu.com.br">
+                        <Img
+                          src={`https://cdn.icon-icons.com/icons2/1195/PNG/512/1490889652-facebook_82510.png`}
+                          width="32"
+                          height="32"
+                          alt="Likedu"
+                          style={socialMediaIcon}
+                        />
+                      </Link>
+                    </Column>
+                    <Column>
+                      <Link href="https://likedu.com.br">
+                        <Img
+                          src={`https://cdn.icon-icons.com/icons2/805/PNG/512/linkedin_icon-icons.com_65929.png`}
+                          width="32"
+                          height="32"
+                          alt="Likedu"
+                          style={socialMediaIcon}
+                        />
+                      </Link>
+                    </Column>
+                  </Row>
+                </Section>
+              </Column>
+            </Row>
+          </Section>
+
+          <Section>
+            <Link
+              style={footerLink}
+              href="https://likedu.com.br"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Policies
+            </Link>
+            &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+            <Link
+              style={footerLink}
+              href="https://likedu.com.br"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Help center
+            </Link>
+            &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+            <Link
+              style={footerLink}
+              href="https://likedu.com/community"
+              target="_blank"
+              rel="noopener noreferrer"
+              data-auth="NotApplicable"
+              data-linkindex="6"
+            >
+              Likedu Community
+            </Link>
+            <Text style={footerText}>
+              ©2022 Likedu <br />
+              <br />
+              All rights reserved.
             </Text>
-            <Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
-            <Text className="text-[12px] leading-[24px] text-slate-500">
-              Likedu
-            </Text>
-          </Container>
-        </Body>
-      </Tailwind>
+          </Section>
+        </Container>
+      </Body>
     </Html>
   );
 };
 
 export default Interest;
+
+const footerText = {
+  fontSize: '12px',
+  color: '#b7b7b7',
+  lineHeight: '15px',
+  textAlign: 'left' as const,
+  marginBottom: '50px',
+};
+
+const footerLink = {
+  color: '#b7b7b7',
+  textDecoration: 'underline',
+};
+
+const footerLogos = {
+  marginBottom: '32px',
+  paddingLeft: '8px',
+  paddingRight: '8px',
+  width: '100%',
+};
+
+const socialMediaIcon = {
+  display: 'inline',
+  marginLeft: '32px',
+};
+
+const main = {
+  backgroundColor: '#ffffff',
+  margin: '0 auto',
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+};
+
+const container = {
+  margin: '0 auto',
+  padding: '0px 20px',
+};
+
+const logoContainer = {
+  marginTop: '32px',
+};
+
+const h1 = {
+  color: '#1d1c1d',
+  fontSize: '36px',
+  fontWeight: '700',
+  margin: '30px 0',
+  padding: '0',
+  lineHeight: '42px',
+};
+
+const heroText = {
+  fontSize: '20px',
+  lineHeight: '28px',
+  marginBottom: '30px',
+};
+
+const text = {
+  color: '#000',
+  fontSize: '14px',
+  lineHeight: '24px',
+};
